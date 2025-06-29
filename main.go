68c8d43cbb351c70c2c235ad13c38f03aed812b4
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-06-28 20:33:05 -0400

    feat: add some middleware and fields to server

--- main.go
+++ main.go
@@ -4,42 +4,57 @@ import (
 	"context"
 	"flag"
 	"log"
+	"log/slog"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
+
+const ServerAddres string = "address"
 
 var addr = flag.String("addr", ":8080", "address for server")
 var disableoptshandler = flag.Bool("disableoptshandler", false, "whether to desable the general options handler")
+var idleTimeout = flag.Int64("i", 0, "Server IdleTimeout property (in seconds)")
+var readHeaderTimeout = flag.Int64("i", 0, "Server ReadHeaderTimeout property (in seconds)")
 
 func main() {
 	flag.Parse()
 	mux := http.NewServeMux()
+	// TODO: determine if writer for logger should be a variable determined by a flag
+	errorLog := slog.NewLogLogger(slog.NewJSONHandler(os.Stderr, nil), slog.LevelError)
+	svrCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	svr := http.Server{
-		Addr:    *addr,
-		Handler: mux,
-		// NOTE: https://www.rfc-editor.org/rfc/rfc7231#section-4.3.7
-		DisableGeneralOptionsHandler: *disableoptshandler,
-		// TODO: Is TLSConfig needed for deploying to AWS, GCP, etc.
-		TLSConfig: nil,
-		// TODO: does TLSNextProto ever need to be set?
-		TLSNextProto:      nil,
-		ReadTimeout:       0,
-		ReadHeaderTimeout: 0,
-		WriteTimeout:      0,
-		IdleTimeout:       0,
+		Addr:              *addr,
+		Handler:           mux,
+		IdleTimeout:       time.Duration(*idleTimeout) * time.Second,
+		ReadHeaderTimeout: time.Duration(*readHeaderTimeout) * time.Second,
 		// NOTE: be default, will use DefaultMaxHeaderBytes;
 		// Go adds an additional 4096 bytes to this maximum as a buffer
 		MaxHeaderBytes: http.DefaultMaxHeaderBytes,
-		ConnState: func(net.Conn, http.ConnState) {
-			panic("TODO")
-		},
-		ErrorLog: &log.Logger{},
-		BaseContext: func(net.Listener) context.Context {
-			panic("TODO")
+		ErrorLog:       errorLog,
+		// TODO: Set up ConnState for monitoring
+		// ConnState: func(net.Conn, http.ConnState) {
+		// 	panic("TODO")
+		// },
+		BaseContext: func(l net.Listener) context.Context {
+			return context.WithValue(svrCtx, ServerAddres, l.Addr().String())
 		},
 		ConnContext: func(ctx context.Context, c net.Conn) context.Context {
 			panic("TODO")
 		},
+		// NOTE: https://www.rfc-editor.org/rfc/rfc7231#section-4.3.7
+		DisableGeneralOptionsHandler: *disableoptshandler,
+		// NOTE: TLSConfig not needed for deploying to AWS, GCP, etc.
+		TLSConfig: nil,
+		// NOTE: only set if you won't use HTTP/2
+		TLSNextProto: nil,
+		// NOTE: handlers should handle their own read and write timeouts
+		WriteTimeout: 0,
+		ReadTimeout:  0,
 		// NOTE: HTTP2 field currently has no effect.
 		// see: https://github.com/golang/go/issues/67813
 		HTTP2: nil,
@@ -49,5 +64,40 @@ func main() {
 		// protocols.SetUnencryptedHTTP2(true)
 		Protocols: nil,
 	}
-	svr.ListenAndServe()
+	l, err := net.Listen("tcp", svr.Addr)
+	if err != nil {
+		slog.Error("failed to listen on network",
+			"network", "tcp", "address", *addr, "error message", err.Error())
+		os.Exit(1)
+	}
+	// run server in a goroutine so we can multiplex between signal and error
+	// handling below.
+	errCh := make(chan error, 1)
+	go func() {
+		slog.Info("Server Started", "network", "tcp", "address", *addr)
+		errCh <- svr.Serve(l)
+	}()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer stop()
+
+	select {
+	case err := <-errCh:
+		if err != nil {
+			log.Fatal(err)
+		}
+	case <-ctx.Done():
+		slog.Error("server shutting down", "error", ctx.Err())
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		err := svr.Shutdown(ctx)
+		if err != nil {
+			slog.Error("failed to shutdown server, exiting anyway", "error", err)
+			os.Exit(1)
+
+		}
+		slog.Info("Server shut down successfully")
+
+	}
 }
---
+++ middleware/middleware.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"io"
+	"log/slog"
+	"net/http"
+)
+
+// TODO: Turn into a struct with a ServeHTTP method to allow the passing in of a logger?
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("start", "method", r.Method, "path", r.URL.Path)
+		defer slog.Info("end", "method", r.Method, "path", r.URL.Path)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func DrainAndClose(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+			_, _ = io.Copy(io.Discard, r.Body)
+			_ = r.Body.Close()
+		},
+	)
+}
